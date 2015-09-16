da71bbbc8fbb837794f32469ea62bd802e2758c5
Alex Hest <a.serzhankov@gmail.com>
2015-09-16 03:38:46 -0400

    Added resources to create user, create jboss folder, download jboss installation archive

--- jboss/attributes/default.rb
+++ jboss/attributes/default.rb
@@ -1,3 +1,4 @@
 default["jboss"]["url"] = "http://download.jboss.org/wildfly/8.0.0.Final/wildfly-8.0.0.Final.tar.gz"
 default["jboss"]["path"] = "/opt/jboss"
 default["jboss"]["user"] = "jboss"
+default["jboss"]["testapp_url"] = "http://www.cumulogic.com/download/Apps/testweb.zip"
--- jboss/recipes/default.rb
+++ jboss/recipes/default.rb
@@ -6,5 +6,37 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe "java_se"
+#include_recipe "java_se"
 
+#Creating JBoss user
+user 'jboss' do
+  comment 'A JBoss user'
+end
+
+#Creating directory
+directory '/opt/jboss' do
+owner 'jboss'
+group 'jboss'
+mode '0755'
+action :create
+end
+
+#Downloading JBoss installation
+remote_file '/opt/jboss/jboss.tar.gz' do
+    force_unlink true
+    source node['jboss']['url']
+    owner 'jboss'
+    group 'jboss'
+    mode '0755'
+    action :create
+end
+
+#Downloading TestWeb app
+#remote_file '/opt/jboss/test.zip' do
+#    force_unlink true
+#    source node['jboss']['testapp_url']
+#    owner 'jboss'
+#    group 'jboss'
+#    mode '0755'
+#    action :create
+#end
