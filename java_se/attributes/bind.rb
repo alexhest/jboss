39055beab584c4854bf770ff339567a3285bf34b
Alex Hest <a.serzhankov@gmail.com>
2015-09-16 02:55:48 -0400

    Adding attributes

---
+++ java_se/attributes/bind.rb
@@ -0,0 +1,9 @@
+default['java_se']['version'] = '1.8.0_60'
+default['java_se']['build'] = '27'
+
+# https://www.oracle.com/webfolder/s/digest/8u60checksum.html
+default['java_se']['sha256']['dmg']['x64'] = '554f4fef08a5ea5b5b6e90cacb62a7a390d94d96c27fa2a5d6b44fc73e45465e'
+default['java_se']['sha256']['exe']['x64'] = '9c46a11f026f1c10bb38b10c511ccf4799420e967f57543b5e0e03c6d9390243'
+default['java_se']['sha256']['exe']['i586'] = '2c2eb85ca8db7853cf3a0b074268a4e9095b0126e9e5a3c4b57d030053b99061'
+default['java_se']['sha256']['tar']['x64'] = 'ebe51554d2f6c617a4ae8fc9a8742276e65af01bd273e96848b262b3c05424e5'
+default['java_se']['sha256']['tar']['i586'] = 'e6a36b458351ed35bd7943739ba93d9a246e08a86433e148ff68b1b40d74c2e5'
---
+++ jboss/attributes/default.rb
@@ -0,0 +1,3 @@
+default["jboss"]["url"] = "http://download.jboss.org/wildfly/8.0.0.Final/wildfly-8.0.0.Final.tar.gz"
+default["jboss"]["path"] = "/opt/jboss"
+default["jboss"]["user"] = "jboss"
